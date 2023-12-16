3667373e200de5fe1d43be0f7527477adf57386f
Kyoqazhi <77191817+Kyoqazhi@users.noreply.github.com>
2023-12-16 19:31:22 +0800

    Add files via upload

---
+++ src/DigitButton.js
@@ -0,0 +1,7 @@
+import {actions} from "./App"
+export default function DigitButton({dispatch,digit}){
+    return ( <button onClick={() => dispatch({ type: actions.ADD_DIGIT, payload: { digit } })} >
+    {digit}
+  </button>
+    )
+}
---
+++ src/OperationButton.js
@@ -0,0 +1,7 @@
+import {actions} from "./App"
+export default function OperationButton({dispatch,operation}){
+    return ( <button onClick={() => dispatch({ type: actions.CHOOSE_OPERATION, payload: { operation } })} >
+    {operation}
+  </button>
+    )
+}
